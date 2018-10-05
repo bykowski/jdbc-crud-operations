c936491b20c654a146cff6ed40309437d5197825
Przemyslaw Bykowski <pkosciej@interia.pl>
2018-10-05 11:01:34 +0100

    removing setters from configuration class

--- src/main/java/config/DBConfig.java
+++ src/main/java/config/DBConfig.java
@@ -12,23 +12,11 @@ public enum DBConfig {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getUser() {
         return user;
     }
 
-    public void setUser(String user) {
-        this.user = user;
-    }
-
     public String getPass() {
         return pass;
-    }
-
-    public void setPass(String pass) {
-        this.pass = pass;
     }
 }
